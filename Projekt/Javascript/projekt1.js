75ea682794e6eca75206c1de195463d559415747
2020b-Surányi István <84459000+b0surist@users.noreply.github.com>
2023-10-06 18:52:50 +0200

    Projekt 1

---
+++ Projekt/Javascript/projekt1.js
@@ -0,0 +1,10 @@
+function darkMode(){
+    var d = document.getElementById("egesz");
+    var b = document.getElementById("test");
+    var s = document.getElementById("rend");
+
+    b.classList.toggle("testt");
+    d.classList.toggle("darksotet");
+    s.classList.toggle("rendd");
+   
+}
